7728b33ca0789f28bec563525ebb392b7e431eaf
jiafie <67643874+jiafie7@users.noreply.github.com>
2025-04-25 20:09:16 +0800

    fix(mainwindow): comment each slot handler

--- mainwindow.h
+++ mainwindow.h
@@ -17,25 +17,44 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
-public:
+  public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
+  private slots:
+    // Updates the maximum value of the duration slider and displays the total video duration
     void onDurationChanged(qint64 duration);
+
+    // Updates the slider and current time display as the video plays
     void onPositionChanged(qint64 position);
+
+    // Opens a file dialog to select a video file and starts playing it
     void on_actionOpen_2_triggered();
+
+    // Updates the playback position when the slider is dragged
     void on_horizontalSlider_Duration_valueChanged(int value);
+
+    // Toggles between play and pause when the play/pause button is clicked
     void on_pushButton_Play_Pause_clicked();
+
+    // Stops the video playback and resets the play/pause button
     void on_pushButton_Stop_clicked();
+
+    // Toggles between mute and unmute when the volume button is clicked
     void on_pushButton_Volume_clicked();
+    
+    // Adjusts the audio volume based on the slider
     void on_horizontalSlider_Volume_valueChanged(int value);
+
+    // Moves the video position 5 seconds back
     void on_pushButton_Seek_Backward_clicked();
+
+    // Moves the video position 5 seconds forward
     void on_pushButton_Seek_Forward_clicked();
 
-private:
+  private:
     Ui::MainWindow *ui;
     QMediaPlayer *Player;
     QAudioOutput *AudioOutput;
